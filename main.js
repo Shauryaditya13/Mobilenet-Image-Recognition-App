4afec06dfe7deddd958a19507e3cfa35761bca24
Shauryaditya13 <71700044+Shauryaditya13@users.noreply.github.com>
2021-05-23 10:41:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,32 @@ constraints:{facingMode:'environment'}
 
 camera=document.getElementById("camera");
 Webcam.attach("camera");
+
+function takesnapshot() {
+    Webcam.snap(function(data_url){
+document.getElementById("result").innerHTML="<img id='capturedimage' src='"+data_url+"'>";
+    });
+}
+
+console.log("ml5version",ml5.version);
+classifier=ml5.imageClassifier('MobileNet',modelloaded);
+
+function modelloaded() {
+    console.log("modelloaded");
+}
+
+function identifyimage() {
+    img=document.getElementById("capturedimage");
+    classifier.classify(img,getresult);
+}
+
+function getresult(error,results){
+    if(error){
+        console.log(error);
+    } 
+    else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML=results[0].label;
+
+    }
+}
